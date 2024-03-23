bcee3f2141edc6257ddeee46ded2cb946a7e15b6
wsprego <wesleysilva.santos076@gmail.com>
2024-03-23 19:48:40 -0300

    add crud
    add post
    add delete
    add put
    add get

--- src/main/java/com/wsprego/api/controllers/ClienteController.java
+++ src/main/java/com/wsprego/api/controllers/ClienteController.java
@@ -2,35 +2,78 @@ package com.wsprego.api.controllers;
 
 import com.wsprego.api.domain.model.Cliente;
 import com.wsprego.api.domain.repository.ClienteRepository;
-import jakarta.persistence.PersistenceContext;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
     private final ClienteRepository clienteRepository;
 
     //listar todos os clientes
-    @GetMapping("/clientes")
+    @GetMapping
     public List<Cliente> listar(){
         return clienteRepository.findAll();
     }
 
     //listar por clientes com esse nome exato
-    @GetMapping("/clientes/nome")
+    @GetMapping("/buscarNome")
     public List<Cliente> listarNome(){
         return clienteRepository.findByNome("João");
     }
 
     //listar por clientes contedo esse nome
-    @GetMapping("/clientes/contendo")
+    @GetMapping("/buscarNomeContendo")
     public List<Cliente> listarNomeContendo(){
         return clienteRepository.findByNomeContaining("Jo");
+    }
+
+    //buacar cliente por id
+    @GetMapping("/{clienteId}")
+    public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId){
+        Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+
+        if(cliente.isPresent()){
+            return ResponseEntity.ok(cliente.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Cliente adicionar(@RequestBody Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
+    @PutMapping("/{clienteId}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId,@RequestBody Cliente cliente){
+        if(!clienteRepository.existsById(clienteId)){
+            return ResponseEntity.notFound().build();
+        }
+
+        cliente.setId(clienteId);
+        cliente = clienteRepository.save(cliente);
+
+        return ResponseEntity.ok(cliente);
+    }
+
+    @DeleteMapping("/{clienteId}")
+    public ResponseEntity<Void> excluir(@PathVariable Long clienteId){
+        if(!clienteRepository.existsById(clienteId)){
+            return ResponseEntity.notFound().build();
+        }
+
+        clienteRepository.deleteById(clienteId);
+
+        return ResponseEntity.noContent().build();
     }
 
 }
